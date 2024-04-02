10cd1ee09979f956e68d163f616c0b82d011f33a
Ifeanyisunday <165680424+Ifeanyisunday@users.noreply.github.com>
2024-04-02 15:06:39 +0100

    assignment 2
    python task

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+number = "Assignment number 2"
+print(number)
+
+valueD = int(input("Enter your value: \n"))
+
+c = 50
+h = 30
+formula = ((2 * c * valueD)/h) ** (1/2)
+
+
+
+print("The square root of sqrt[(2 * c * valueD)/h] is =", int(formula))
