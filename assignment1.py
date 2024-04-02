461aa725788410ae9b8594ac858ea1ee1abce03e
Ifeanyisunday <sundayjnr10@gmail.com>
2024-04-02 15:03:30 +0100

    initiated file into git

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+number = "Assignment number 1 price and discount"
+print(number) 
+
+
+price = int(input("Enter price: "))
+discount_percent = float(input("Enter Discount percentage: "))
+
+discount = price * discount_percent
+
+get_discount = price - discount
+
+print(get_discount)
