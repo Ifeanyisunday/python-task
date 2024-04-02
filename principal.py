eafbb97133f3a2ac31dd2ede5f46bc5b969a8691
Ifeanyisunday <165680424+Ifeanyisunday@users.noreply.github.com>
2024-04-02 17:17:30 +0100

    compound  interest
    python assignment

---
+++ principal.py
@@ -0,0 +1,18 @@
+print("")
+principal = int(input("Enter the principal amount: "))
+print("")
+assume_interest = int(input("Enter the annual interest rate: "))
+print("")
+duration = int(input("Enter the duration in years: "))
+print("")
+
+annual_interest = assume_interest / 100
+monthly_rate = annual_interest / 12
+number_of_months_in_years = duration * 12
+
+monthly_exponent = (1 + monthly_rate)**number_of_months_in_years
+
+monthly_payment = principal * (monthly_rate * monthly_exponent) / (monthly_exponent - 1)
+
+
+print("Your monthly payment is $" + round(monthly_payment, 2))
