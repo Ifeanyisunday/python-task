370eb7361b4ea1b88cba42c8e3a8b23d2d3ace3b
Ifeanyisunday <165680424+Ifeanyisunday@users.noreply.github.com>
2024-04-06 09:58:21 +0100

    Update positivecount.py

--- positivecount.py
+++ positivecount.py
@@ -6,13 +6,11 @@ zeros = 0
 
 for number in range(userinput):
 	integer = int(input('Enter numbers: '))
-	if userinput >= 1:
+	if integer >= 1:
 		positive = positive + 1
-	elif userinput <= -1:
+	elif integer <= -1:
 		negative = negative + 1
-	else userinput == 0:
+	elif integer == 0:
 		zeros = zeros + 1
 
-print("Number of Positive, negetive and zeros are: ",positive, negative,zeros)
-		
-	
+print("Number of Positive, negetive and zeros are: ",positive, negative, zeros)
